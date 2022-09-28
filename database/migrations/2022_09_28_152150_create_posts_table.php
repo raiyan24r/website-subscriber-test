ed7221157bf2fda7dab9b1cab6492adef7da3ce2
raiyan24r <raiyan24r@gmail.com>
2022-09-28 21:32:49 +0600

    first commit

---
+++ database/migrations/2022_09_28_152150_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->unsignedBigInteger('website_id');
+            $table->timestamps();
+
+            $table->foreign('website_id')->references('id')->on('websites');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
